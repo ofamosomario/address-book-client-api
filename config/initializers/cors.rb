6b2a728d0e72927a01092ed9b179d58646280edc
mario <tkyakow@gmail.com>
2021-02-17 09:38:09 -0300

    change rails cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,11 +4,9 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    
     origins '*'
-
-    # Make sure to change the * to the location of you frontend after deployment in order to secure your application
-    resource '*', headers: :any, methods: %i[get post put patch delete options head]
-  
+    resource '*', 
+        :headers => :any, 
+        :methods => [:get, :post, :delete, :put, :patch, :options, :head]
   end
 end
