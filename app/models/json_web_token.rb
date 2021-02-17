7ca2aaf04236c6f8e68f63c81ce00b9caabbf16c
mario <tkyakow@gmail.com>
2021-02-17 10:19:52 -0300

    secret key.

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,16 +1,18 @@
 class JsonWebToken
+
+  JWT_SECRET = ENV["JWT_SECRET"]
   
   def self.encode payload
 
     expiration = 2.weeks.from_now.to_i
 
-    JWT.encode payload.merge(exp: expiration), Rails.application.credentials.fetch(:secret_key_base)
+    JWT.encode payload.merge(exp: expiration), JWT_SECRET
 
   end
 
   def self.decode token
 
-    JWT.decode(token, Rails.application.credentials.fetch(:secret_key_base)).first
+    JWT.decode(token, JWT_SECRET).first
     
   end
 
